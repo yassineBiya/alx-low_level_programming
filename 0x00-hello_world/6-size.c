b4d2289f6a96fd2b2096f735501d87d9bf9313a7
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-15 15:54:36 -0700

    c sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
