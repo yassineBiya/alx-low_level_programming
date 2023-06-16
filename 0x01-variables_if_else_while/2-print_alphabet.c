758a60f4576e8186882a30882a040fac0a58e49f
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-16 15:35:59 -0700

    alpha lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - To print the alphabets in lowercase
+ * followed by newline.
+ *
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alpha[26]="abcdefghijklmnopkrstuvwxyz";
+	int i;
+	
+	for (i=0; i<26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
