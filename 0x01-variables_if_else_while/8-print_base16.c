024e923213191f2a61cc1593dc39c8da41355855
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-16 16:26:28 -0700

    nbrs in low base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - To print the numbers base 16 in low using putchar
+ * followed by newline.
+ *
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10) {
+		putchar(i + '0');   /*digits 0-9*/
+		}
+		else {
+			putchar(i -10 + 'a');	/*letter a-f*/		
+		}
+	}
+	putchar('\n');
+	return (0);
+}
