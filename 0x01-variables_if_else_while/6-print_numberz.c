6c205f5cba271b805d2973d1e6d0e3e8592fad11
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-16 16:10:42 -0700

    0 2 9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - To print the numbers btwn 0 & 9 using putchar
+ * followed by newline.
+ *
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
