55e7f5d777409ce87f75ce5bb73c9281c589313a
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-16 15:53:38 -0700

    0 2 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - To print the alphabets in lowercase except q & e
+ * followed by newline.
+ *
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf('\n');
+	return (0);
+}
