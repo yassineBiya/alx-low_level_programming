fb760749f035b4e5119f503c6dc309be46f865a1
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-16 16:16:54 -0700

    low alpha in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - To print the alphabets in lowercase in reverse
+ * followed by newline.
+ *
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 26; i > 0; i--)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
