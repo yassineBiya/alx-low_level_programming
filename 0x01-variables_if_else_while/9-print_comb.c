9449f5c2dcc1149e9fbb28286b9189e83957c3fc
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-16 16:40:41 -0700

    combinations of signle digits using putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - To print every combination of single digits using putchar
+ * followed by newline.
+ *
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+        if (i != 9)
+		{
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    
+    putchar('\n');    
+    return (0);
+}
