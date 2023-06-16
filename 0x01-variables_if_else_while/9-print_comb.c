13931c2be1b46afea0d1d83ab4e0a5a7c546b35d
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-16 16:48:15 -0700

    combinations of signle digits using putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
@@ -21,7 +21,7 @@ int main(void)
 			putchar(' ');
 		}
 	}
-	
-	putchar('\n');    
+
+	putchar('\n');
 	return (0);
 }
