feb832ee8c51d624056be02a92e123580bf12feb
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-20 16:07:10 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - To print _putchar
+ *
+ *
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0); 
+}
