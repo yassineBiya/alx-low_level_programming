e02672819b40e4eb36e4e8af9cfe3a82f91843e7
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-21 15:32:25 -0700

    fibo even valued

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,14 +6,14 @@
  */
 #include <stdio.h>
 
-long int evenFibSum(long int limit) {
+unsigned long evenFibSum(unsigned long limit) {
   if (limit < 2) {
     return 0;
   }
 
-  long int ef1 = 0, ef2 = 2, sum = ef1 + ef2;
+  unsigned long ef1 = 0, ef2 = 2, sum = ef1 + ef2;
   while (ef2 <= limit) {
-    long int ef3 = 4 * ef2 + ef1;
+    unsigned long ef3 = 4 * ef2 + ef1;
     if (ef3 > limit) {
       break;
     }
@@ -27,8 +27,8 @@ long int evenFibSum(long int limit) {
 }
 
 int main() {
-  long int limit = 4000000;
-  long int sum = evenFibSum(limit);
-  printf("The sum of the even-valued Fibonacci terms less than or equal to %ld is %ld.\n", limit, sum);
+  unsigned long limit = 4000000;
+  unsigned long sum = evenFibSum(limit);
+  printf("The sum of the even-valued Fibonacci terms less than or equal to %lu is %lu.\n", limit, sum);
   return 0;
 }
