8d024e4e681198eaefa345e4b850191008a4386c
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-22 16:34:37 -0700

    more numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,6 +9,13 @@ int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char ch);
 void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
 
 
 #endif /* MAIN_H */
