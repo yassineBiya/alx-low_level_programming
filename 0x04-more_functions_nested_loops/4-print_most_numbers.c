5015df402bfd1411e748b1299a8e9fca8d080bb3
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-22 16:31:15 -0700

    excpet 2&4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - to print the numbers, from 0 to 9, except 2 & 4
+ * followed by a new line
+ *
+ * return: void
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char ch);
+void print_most_numbers(void);
 
 
 #endif /* MAIN_H */
