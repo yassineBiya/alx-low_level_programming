459bcdc639a212f0b5a013a921d0b8693571b863
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-26 05:04:25 -0700

    cp src dest

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -24,6 +24,8 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
 
 
 #endif /* MAIN_H */
