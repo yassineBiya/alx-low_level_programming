82b8cfe3b3cece38b0d0e07d4fb5bb137fbb49f5
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-26 05:06:13 -0700

    cp src dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * char *_strcpy - copies the string pointed to by src, 
- * including the terminating null byte (\0), 
+ * char *_strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
  * to the buffer pointed to by dest.
  *
  * @dest: cp destination.
