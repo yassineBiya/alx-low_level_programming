ba78d467a82c27725013fa0131eb14149d8b0915
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-26 04:52:48 -0700

    print 2nd half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,8 +2,8 @@
 /**
  * puts_half - prints second half of a string, followed by a new line.
  *
- * If the number of characters is odd, 
- * the function should print the last n characters of the string, 
+ * If the number of characters is odd,
+ * the function should print the last n characters of the string,
  * where n = (length_of_the_string - 1) / 2
  *
  * @str: input.
