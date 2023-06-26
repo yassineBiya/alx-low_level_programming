82d54a2a88f536d5d5b4f656938a76be48da8d3a
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-26 04:15:19 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,7 +15,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-void reset_to_98(int *n);
+void reset_to_98(int* n);
 
 
 #endif /* MAIN_H */
