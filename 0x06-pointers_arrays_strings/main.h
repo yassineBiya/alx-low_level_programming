77d7aa212ab21dec2b3018da6e1dd3cc6ae37e96
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-29 16:35:24 -0700

    strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -27,6 +27,7 @@ void print_array(int *a, int n);
 int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 
 #endif /* MAIN_H */
