58d4d2c0607368b6d18388c12921070dbe1de822
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-29 16:51:20 -0700

    strncpy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -28,6 +28,7 @@ int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 
 #endif /* MAIN_H */
