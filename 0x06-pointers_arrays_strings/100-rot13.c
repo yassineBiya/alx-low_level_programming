0f171990f7926c4d1502f3575c9d991b13a81a0a
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-29 17:35:36 -0700

    rot encode

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13.
+ *
+ * @s: input string
+ *
+ * Return: pointer to new encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char arr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == arr[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+	return (s);
