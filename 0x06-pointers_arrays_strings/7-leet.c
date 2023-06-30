90641567dbec1bc9473619a9f4e018e028cf375d
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-29 17:26:36 -0700

    str encod

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+char *leet(char *s)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == s1[j])
+			{
+				s[i] = s2[j];
+			}
+		}
+	}
+	return (s);
+} 
+
+
