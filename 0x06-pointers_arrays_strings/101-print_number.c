d92fe178e0ee4f3a2c0eb2a0da93f0b6ebd24fe2
Yassine Biyaali <melbiyaali@gmail.com>
2023-06-29 17:44:42 -0700

    print int

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ *
+ * @n: integer to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int tmp;
+	tmp = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		tmp = -n;
+	}
+
+	if (tmp / 10 != 0)
+	{
+		print_number(tmp / 10);
+	}
+	_putchar((tmp % 10) + '0');
+}
