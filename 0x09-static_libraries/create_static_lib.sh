e61cadeb1e3a18c72b2a004ca8f8e391dedd4dd8
Yassine Biyaali <melbiyaali@gmail.com>
2023-07-07 18:01:14 -0700

    script stat lib

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,10 @@
-i#!/bin/bash
-gcc -c *.c
-ar rcs liball.a *.o
-ranlib liball.a
+#!/bin/sh
+
+# Get the list of all .c files in the current directory
+files=$(find . -name "*.c")
+
+# Create the static library
+ar cr liball.a $files
+
+# Print a message to indicate that the library was created successfully
+echo "Static library liball.a created successfully"
