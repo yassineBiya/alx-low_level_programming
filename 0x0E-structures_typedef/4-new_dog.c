0b807187bf6cafdea461a1fe2d0c3b471aeae65b
Yassine Biyaali <melbiyaali@gmail.com>
2023-07-17 18:03:33 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,52 @@
 #include <stdlib.h>
 #include "dog.h"
 #include "main.h"
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+		int leng = 0;
+
+		while (*s != '\0')
+		{
+			leng++;
+			s++;
+		}
+
+		return (leng);
+}
+#include "main.h"
+/**
+ * char *_strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ *
+ * @dest: cp destination.
+ * @src: cp source.
+ *
+ * Return: string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+		int l = 0;
+		int m = 0;
+
+		while (*(src + l) != '\0')
+		{
+			l++;
+		}
+		for ( ; m < l ; m++)
+		{
+			dest[m] = src[m];
+		}
+		dest[l] = '\0';
+		return (dest);
+}
 /**
  * new_dog - creates a new dog.
  *
