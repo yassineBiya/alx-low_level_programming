88f59ab8f4cbb1fb1b7521b5794367f0bbc07458
Yassine Biyaali <melbiyaali@gmail.com>
2023-07-17 17:25:08 -0700

    poppy struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's basic info
+ *
+ * @name: First mbr
+ * @age: Second mbr
+ * @owner: Third mbr
+ *
+ * Description: Dof info
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
