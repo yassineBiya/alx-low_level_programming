afd6aecdc069c04c1d34aa27f1462e4e59345f9d
Yassine Biyaali <melbiyaali@gmail.com>
2023-07-28 18:15:54 -0700

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,17 @@
 section .data
-    hello db "Hello, Holberton", 0  ; Null-terminated string to print
+	hello db "Hello, Holberton", 0  ; Null-terminated string to print
 
 section .text
-    extern printf                 ; Declare the external reference to printf
-    global main                   ; Entry point of the program
+	extern printf                 ; Declare the external reference to printf
+	global main                   ; Entry point of the program
 
 main:
-    ; --- Prepare the arguments for printf ---
-    mov rdi, hello                ; Set the format string (1st argument) to hello
-    xor rax, rax                  ; Clear rax to indicate that there are no SSE arguments
-    call printf                   ; Call printf with the format string
+	; --- Prepare the arguments for printf ---
+	mov rdi, hello                ; Set the format string (1st argument) to hello
+	xor rax, rax                  ; Clear rax to indicate that there are no SSE arguments
+	call printf                   ; Call printf with the format string
 
-    ; --- Exit the program ---
-    xor rdi, rdi                  ; Set the exit code to 0
-    mov rax, 60                   ; syscall number for sys_exit (60)
-    syscall                       ; Invoke the syscall to exit
-
-section .bss
-    ; No need for a .bss section in this program
+	; --- Exit the program ---
+	xor rdi, rdi                  ; Set the exit code to 0
+	mov rax, 60                   ; syscall number for sys_exit (60)
+	syscall                       ; Invoke the syscall to exit
