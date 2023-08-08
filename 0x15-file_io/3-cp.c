e036234df8aa993b7be48fa02528a62dd4cb6f02
Yassine Biyaali <melbiyaali@gmail.com>
2023-08-07 18:19:29 -0700

    cp file content to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -65,7 +65,8 @@ int main(int argc, char *argv[])
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	do {
+	while (r > 0)
+	{
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
@@ -86,7 +87,7 @@ int main(int argc, char *argv[])
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (r > 0);
+	}
 
 	free(buffer);
 	close_file(from);
